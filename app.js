d569d3160c2c5fbc169176076ec230974b00487d
Garrad Belle <garrad.d.belle@gmail.com>
2021-08-17 17:16:26 -0400

    accessing div within grid class

--- app.js
+++ app.js
@@ -3,13 +3,24 @@
 //document.addEventListener('DOMContentLoaded', () => { // <= ensures that the script doesn't launch until the html/DOM has fully loaded
     // **Code goes here**
 
-const gBox = (numberOfSquares) => {
+const gBox = (numberOfSquares) => { // <-- Function used for creating the "game board" 
     for(i=0; i < numberOfSquares; i++){
-        let square = $('<div>').addClass('square');
-        square.addClass('background-color');
-        $('body').append(square);
+        let grid = $('<div>').addClass('grid');
+        grid.addClass('background-color');
+        $('body').append(grid);
         }
 }
 
-$( () => gBox(200))
+const sBox = (numberofBoxes) => {
+    for(i=0; i < numberofBoxes; i++){
+        let square = $('<div>').addClass('square') ;
+        square.addClass('background-color');
+        $('grid').append(square);
+        }
+}
+
+
+$(() => gBox(200)) // <-- This is an "IIFE." It allows a function to be called 
+//                       w/o "hard coding" it into the index
+$(() => sBox(200))
 //})
