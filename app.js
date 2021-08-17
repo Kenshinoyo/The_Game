4ee51c112761705c1fe966c26e127f74b591cfad
Garrad Belle <garrad.d.belle@gmail.com>
2021-08-17 11:21:02 -0400

    Rough Draft(Start)

---
+++ app.js
@@ -0,0 +1,12 @@
+// *TETRIS TEMPLATE*
+
+document.addEventListener('DOMContentLoaded', () => { // <= ensures that the script doesn't launch until the html/DOM has fully loaded
+    // **Code goes here**
+
+const gBox = (numberOfSquares) => {
+    for(i=0; i < numberOfSquares; i++){
+        let square = $('div').addClass('square');
+        $('body').append(square);
+        }
+}
+})
