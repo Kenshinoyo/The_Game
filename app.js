08bd8e7c73cee8cc052044f19827806ec602fd6a
Garrad Belle <garrad.d.belle@gmail.com>
2021-08-18 14:37:14 -0400

    take 2

--- app.js
+++ app.js
@@ -3,26 +3,19 @@
 //document.addEventListener('DOMContentLoaded', () => { // <= ensures that the script doesn't launch until the html/DOM has fully loaded
     // **Code goes here**
 
-const gBox = (numberOfSquares) => { // <-- Function used for creating the "game board" 
-    for(i=0; i < numberOfSquares; i++){
-        let grid = $('<div>').addClass('grid');
-        let box = document.createElement('<div>')
-        grid.addClass('background-color');
-        $('body').append(grid);
-        $('grid').append(box);
-        }
-}
-
-// const sBox = (numberofBoxes) => {
-//     for(i=0; i < numberofBoxes; i++){
-//         let square = $('<div>').addClass('square');
-//         square.addClass('background-color');
-//         $('grid').append(square);
+// const gBox = (numberOfSquares) => { // <-- Function used for creating the "game board" 
+//     for(i=0; i < numberOfSquares; i++){
+//         let grid = $('<div>').addClass('grid');
+//         grid.addClass('background-color');
+//         $('body').append(grid);
 //         }
 // }
 
+// const sBox = (numberOfBoxes) => {
+// for(i = 0; i < numberOfBoxes; i++) 
 
-$(() => gBox(200)) // <-- This is an "IIFE." It allows a function to be called 
+// }
+// $(() => gBox(200)) // <-- This is an "IIFE." It allows a function to be called 
 //                       w/o "hard coding" it into the index
-$(() => sBox(200))
+
 //})
