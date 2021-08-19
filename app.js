bd37fa3c506f07b83d9c36ef023dfe26937bb59b
Garrad Belle <garrad.d.belle@gmail.com>
2021-08-19 16:35:30 -0400

    child success

--- app.js
+++ app.js
@@ -3,24 +3,25 @@
 //document.addEventListener('DOMContentLoaded', () => { // <= ensures that the script doesn't launch until the html/DOM has fully loaded
     // **Code goes here**
 
-const gBox = (numberOfSquares) => { // <-- Function used for creating the "game board" 
+const sBox = (numberOfSquares) => { // <-- Function used for creating the "game board" 
     for(i=0; i < numberOfSquares; i++){
-        let grid = $('<div>').addClass('grid');
-        grid.addClass('background-color');
-        $('body').append(grid);
+        let box = $('<div>');
+        box.addClass('box');
+        box.attr("id", i)
+        $('.gBoard').append(box);
         }
 }
 
 
-$(() => gBox(1)) // <-- This is an "IIFE." It allows a function to be called 
+$(() => sBox(198)) // <-- This is an "IIFE." It allows a function to be called 
 //                       w/o "hard coding" it into the index
+ 
 
-
-// const grid = document.querySelector('.grid')
-// let squares = array.from(document.querySelectorAll('.grid div'))
-// const ScoreDisplay = document.querySelectorAll('#score')
-// const StartBtn = document.querySelector('#start-button')
-// const width = 10
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.box'))
+const ScoreDisplay = document.querySelectorAll('#score')
+const StartBtn = document.querySelector('#start-button')
+const width = 10
 
 
 // $(document).ready(gBox(numberOfSquares){
@@ -30,18 +31,18 @@ $(() => gBox(1)) // <-- This is an "IIFE." It allows a function to be called
 // })
 
 
-var toAdd = document.createDocumentFragment();
+// var toAdd = document.createDocumentFragment();
 
-const sBox = (numberOfBoxes) => { // <-- Function for adding child divs to "grid" class
-    for(i = 0; i < numberOfBoxes; i++){
-    let newDiv = document.createElement('div');
-        toAdd.appendChild(newDiv);
-    }
+// const sBox = (numberOfBoxes) => { // <-- Function for adding child divs to "grid" class
+//     for(i = 0; i < numberOfBoxes; i++){
+//     let newDiv = document.createElement('div');
+//         toAdd.appendChild(newDiv);
+//     }
     
-    document.getElementsByClassName('.grid').appendChild(toAdd);
-}
+//     document.getElementsByClassName('.grid').appendChild(toAdd);
+// }
 
-$(() => sBox(200))
+// $(() => sBox(200))
 
 // }
 // const sBox = (numberOfBoxes) => {
